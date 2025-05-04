ab7839b88c824282fd2f86fc73ebc405e8bba9bc
trankhiem <trankhiem2003@gmail.com>
2025-05-05 02:28:04 +0700

    add:AddBackTRack

--- TSP_timewindown.py
+++ TSP_timewindown.py
@@ -22,6 +22,7 @@ def checkIfValid_Node(node, start, end, arrival_time, d_time):
 
 def backtrack(cur_step, path, visited, cur_time, start, end, d_time, times, n, best_path, min_time):
     print("cur_step: ",cur_step)
+    print("cur_time: ",cur_time)
     # Base case: all nodes visited
     if cur_step == n:
         if cur_time + times[path[cur_step]][0] < min_time:  # Using list to allow modification
@@ -31,7 +32,7 @@ def backtrack(cur_step, path, visited, cur_time, start, end, d_time, times, n, b
     
     
     prev_node = 0 if cur_step == 1 else path[cur_step-1]
-    for node in range(1, n+1):
+    for node in range(1, n+1):                                                                                           
         if not visited[node]:
             travel_time = times[prev_node][node] 
             arrival_time = cur_time + travel_time
@@ -41,7 +42,7 @@ def backtrack(cur_step, path, visited, cur_time, start, end, d_time, times, n, b
                 visited[node] = True
                 path[cur_step] = node
                 new_time = arrival_time + d_time[node-1]
-                
+                print("cur path:", path)
                 backtrack(cur_step + 1, path, visited, new_time, 
                          start, end, d_time, times, n, best_path, min_time)
                 
@@ -51,11 +52,9 @@ def backtrack(cur_step, path, visited, cur_time, start, end, d_time, times, n, b
 def main():
     n, start, end, d_time, times = read_input()
     visited = [False] * (n + 1)
-    path = [0] * n  # Only need n slots since we visit n nodes
+    path = [0] * (n+1)  
     best_path = [0] * n
-    min_time = float('inf')  # Using list to allow modification in recursive calls
-    
-   
+    min_time = float('inf')  
     visited[0] = True
 
     backtrack(1, path, visited, 0, start, end, d_time, times, n, best_path, min_time)
---
+++ backTracking.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+int n;
+int e[1000], l[1000], d[1000];
+int cost[1000][1000];
+int path[1000];
+bool Visited[1000] = {};
+int cur_time = 0;
+int Min_Time = __INT_MAX__;
+int solution[1000];
+int min_cost = __INT_MAX__;
+int min_d = __INT_MAX__;
+bool CheckIfValid(int cur_node, int cur_time, int last_node){
+    if (!Visited[cur_node] && cur_time + cost[last_node][cur_node] + d[cur_node] <= l[cur_node] && cur_time +cost[last_node][cur_node]+ d[cur_node] >= e[cur_node]){
+        return true;
+    }
+    return false;
+}
+void backTrack(int cur_step){
+    int last_node = path[cur_step-1];
+
+    for(int i = 1; i <=n ; i++){
+        if (CheckIfValid(i,cur_time,last_node)){
+            cur_time += cost[last_node][i] + d[i];
+            Visited[i] = true;
+            path[cur_step] = i;
+            if(cur_step == n){
+                if (cur_time + cost[i][0] < Min_Time){
+                    Min_Time = cur_time + cost[i][0];
+                    for(int j = 1; j<=n ; j++){
+                        solution[j] = path[j];
+                    }
+                }
+            }else{
+                if(cur_time + (n - cur_step)*(min_d + min_cost) + min_cost < Min_Time){
+                    backTrack(cur_step + 1);
+                }
+            }
+            cur_time -= (cost[last_node][i] + d[i]);
+            Visited[i] = false;
+            path[cur_step] = 0;
+        }
+    }
+}
+int main(){
+    cin>>n;
+    for (int i=1; i<=n; i++){
+       cin>>e[i]>>l[i]>>d[i];
+       min_d = min(d[i], min_d);
+    }
+    for (int i=0; i<=n ; i++){
+        for (int j=0; j<=n ; j++){
+            cin>>cost[i][j];
+            min_cost = min(min_cost,cost[i][j]);
+        }
+    }
+    backTrack(1);
+    cout<<n<<endl;
+    for(int i = 1; i<=n ; i++ ){
+        cout<<solution[i]<<" ";
+    }
+    return 0;
+
+}
